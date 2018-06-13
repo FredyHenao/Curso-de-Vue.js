49fea6a2d6ebb0a73c891bba6f1badf4035a40ca
FredyHenao <jhongallego45y@hotmail.com>
2018-06-13 18:10:53 -0500

    Directivas

---
+++ directivas.js
@@ -0,0 +1,16 @@
+/*directivas son atributos que dan o proporcionan capacidades extra
+a elementos html
+*/
+
+/**
+ * renderizado condicional
+ * https://vuejs.org/v2/guide/conditional.html
+ */
+
+const vm = new Vue({
+    el: 'main',
+    data:{
+        conectado: false,
+        edad: 45
+    }
+});
