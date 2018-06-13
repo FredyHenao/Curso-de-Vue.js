192240f094b792796e3236a846cd83e6f9696bed
FredyHenao <jhongallego45y@hotmail.com>
2018-06-13 18:40:01 -0500

    Renderizado de listas

---
+++ renderizado-de-listas/main.js
@@ -0,0 +1,12 @@
+const vm = new Vue({
+   el: 'main',
+   data:{
+       laborales: ['Lunes','Martes','Miercoles','Jueves','Viernes'],
+       persona: {
+        nombre: 'Fredy',
+        profesion: 'Ingeniero',
+        ciudad: 'Bogotá'
+        }
+   }
+   
+});
